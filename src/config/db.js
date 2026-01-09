59382c1f0e12111306815682e965959581e4a81a
Eunock <izumishinishi7@gmail.com>
2026-01-09 20:40:32 +0100

    Mis en place de la base de donnée

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient({
+    datasource: {
+        url: process.env.DATABASE_URL
+    },  
     log: 
     process.env.NODE_ENV === 'development' 
         ? ['query', 'error', 'warn'] 
