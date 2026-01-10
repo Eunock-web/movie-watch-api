c9ec4ab304f58b7eb1f8e355d4e68962d4403ae2
Eunock <izumishinishi7@gmail.com>
2026-01-10 12:51:44 +0100

    feat: controller du register

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,9 +1,42 @@
-
+import { prisma } from '../config/db.js'
+import bcrypt from 'bcryptjs';
 
 const regsiterController = async (req, res)=>{
-    res.json({
-        message : "It works"
+    const {name, email, password} = req.body;
+
+    //Check if user already exist
+    const userExist = await prisma.user.findUnique({
+        where: {email: email}
     })
+
+    if(userExist){
+        return res.status(400).json({
+            error : "User with this email already exist"
+        })
+    }
+
+    //Hash of password
+    const salt = await bcrypt.genSalt(10);
+    const HashPassword  = await bcrypt.hash(password, salt);
+
+    //Create user
+    const user = prisma.user.create({
+        data : {
+            name,
+            email,
+            password : HashPassword
+        }
+    });
+
+    return res.status(201).json({
+        data : {
+            user : {
+                id : user.id,
+                name : name,
+                email : email,
+            }
+        }
+    });
 }
 
 
